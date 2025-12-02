259c067bb392e616209b41ffc378cadb68213845
Evaldas Visockas <vievaldas@gmail.com>
2025-12-02 21:39:40 +0200

    Add database migration

---
+++ backend/migrations/20251202171654-save-drawing.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  await db.runSql(`CREATE SCHEMA drawings;`);
+
+  await db.runSql(`CREATE EXTENSION postgis;`);
+
+  await db.runSql(`
+    CREATE TABLE drawings.drawings (
+      id SERIAL PRIMARY KEY,
+      owner VARCHAR(100) NOT NULL,
+      title VARCHAR(255),
+      drawing geometry(MultiPolygon, 4326) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+
+  await db.runSql(`
+    CREATE INDEX idx_drawings_drawing
+    ON drawings.drawings
+    USING GIST (drawing);
+  `);
+};
+
+exports.down = async function(db) {
+  await db.runSql(`DROP TABLE drawings.drawings;`);
+  await db.runSql(`DROP SCHEMA drawings CASCADE;`);
+};
+
+exports._meta = {
+  "version": 1
+};
